3d834eb969ff2521c630ccc03da39040a02e4290
orestdzhus <work.orest.dzhus@gmail.com>
2023-11-14 14:59:57 +0200

    test the dropdown

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,25 +3,28 @@ import DropdownMenu from "@/components/DropdownMenu/DropdownMenu";
 
 export default function Home() {
     return (
-        <div className="bg-neutral-500 h-screen flex justify-center items-center">
-
-            <div className="h-[800px] bg-blue-900 w-[500px]">
-                <div className="border-2 border-orange-600 h-1/2 flex justify-center">
-                    <div className="">
-                        <img className="h-[200px] w-[200px]" src="/img.png" alt="logo"/>
-                    </div>
-                </div>
-                <div className="border-2 border-orange-600 h-1/2 flex items-center flex-col   ">
-                    <div className="flex flex-col justify-around w-3/5  h-2/6">
-                        <input className="text-2xl" type="text" defaultValue={100}/>
-                        <input className="text-2xl" type="text" defaultValue={100}/>
-                    </div>
-                    <div className="pt-[30px] w-3/5">
-                        <ConnectWalletButton/>
-                    </div>
-                </div>
-            </div>
+        <div className="h-screen grid place-items-center bg-gradient-to-r from-blue-950 to-slate-700">
+            <DropdownMenu/>
         </div>
+        // <div className="bg-neutral-500 h-screen flex justify-center items-center">
+        //
+        //     <div className="h-[800px] bg-blue-900 w-[500px]">
+        //         <div className="border-2 border-orange-600 h-1/2 flex justify-center">
+        //             <div className="">
+        //                 <img className="h-[200px] w-[200px]" src="/img.png" alt="logo"/>
+        //             </div>
+        //         </div>
+        //         <div className="border-2 border-orange-600 h-1/2 flex items-center flex-col   ">
+        //             <div className="flex flex-col justify-around w-3/5  h-2/6">
+        //                 <input className="text-2xl" type="text" defaultValue={100}/>
+        //                 <input className="text-2xl" type="text" defaultValue={100}/>
+        //             </div>
+        //             <div className="pt-[30px] w-3/5">
+        //                 <ConnectWalletButton/>
+        //             </div>
+        //         </div>
+        //     </div>
+        // </div>
     );
 };
 
--- src/components/DropdownMenu/DropdownMenu.tsx
+++ src/components/DropdownMenu/DropdownMenu.tsx
@@ -1,7 +1,50 @@
-export default function DropdownMenu() {
-    return (
-        <div>
+"use client";
+import {useState} from "react";
+import {AiOutlineCaretDown, AiOutlineCaretUp} from "react-icons/ai";
+import {list} from "postcss";
 
+export default function DropdownMenu() {
+
+    const list = [
+        {city: "NEW YORK"},
+        {city: "NEW YORK"},
+        {city: "NEW YORK"},
+        {city: "NEW YORK"},
+        {city: "NEW YORK"},
+        {city: "NEW YORK"},
+    ];
+
+    const [isOpen, setIsOpen] = useState(false);
+
+    console.log(isOpen);
+
+
+    return (
+        <div className="relative flex flex-col items-center w-[340px] h-[340px]">
+            <button onClick={() => setIsOpen((prev) => !prev)}
+                    className="bg-blue-300 p-4 w-full flex items-center justify-between font-bold text-lg tracking-wider border-4
+                    border-transparent active:border-white duration-300 active:text-white"
+            >
+                Dropdown
+                {!isOpen ? (
+                    <AiOutlineCaretDown className="h-8"/>
+                ) : (
+                    <AiOutlineCaretUp className="h-8"/>
+                )}
+            </button>
+
+            {isOpen && (
+                <div className="bg-blue-400 absolute top-20 w-full">
+                    {list.map((item, index) => (
+                        <div>
+                            <h3>
+                                {item.city}
+                            </h3>
+                        </div>
+                    ))
+                    }
+                </div>
+            )}
         </div>
     );
 };
