2dae703e83d91ef78aa36266070a16606ddfd4a2
orestdzhus <work.orest.dzhus@gmail.com>
2023-11-14 17:59:29 +0200

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,46 @@
+"use client";
+
 import ConnectWalletButton from "@/components/ConnectWalletButton/ConnectWalletButton";
 import DropdownMenu from "@/components/DropdownMenu/DropdownMenu";
+import {useEffect, useState} from "react";
 
 export default function Home() {
+
+    const [coinList, setCoinList] = useState([]);
+
+    useEffect(() => {
+        fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc")
+            .then(res => res.json())
+            .then(value => setCoinList(value));
+    }, []);
+
     return (
-        <div className="h-screen grid place-items-center bg-gradient-to-r from-blue-950 to-slate-700">
-            <DropdownMenu/>
+        <div className="bg-neutral-500 h-screen flex justify-center items-center">
+
+            <div className="h-[800px] w-[500px]">
+                <div className="h-1/2 flex justify-center">
+                    <div className="">
+                        <img className="h-[200px] w-[200px]" src="/img.png" alt="logo"/>
+                    </div>
+                </div>
+
+                <div className="h-1/2 flex items-center flex-col   ">
+                    <div className="flex flex-col justify-around w-3/5  h-2/6">
+                        <input className="text-2xl" type="text" defaultValue={100}/>
+                        <input className="text-2xl" type="text" defaultValue={100}/>
+                    </div>
+                    <div className="pt-[30px] w-3/5">
+                        <ConnectWalletButton/>
+                    </div>
+                </div>
+
+            </div>
+
+            <div>
+                <DropdownMenu coinList={coinList}/>
+            </div>
+
         </div>
-        // <div className="bg-neutral-500 h-screen flex justify-center items-center">
-        //
-        //     <div className="h-[800px] bg-blue-900 w-[500px]">
-        //         <div className="border-2 border-orange-600 h-1/2 flex justify-center">
-        //             <div className="">
-        //                 <img className="h-[200px] w-[200px]" src="/img.png" alt="logo"/>
-        //             </div>
-        //         </div>
-        //         <div className="border-2 border-orange-600 h-1/2 flex items-center flex-col   ">
-        //             <div className="flex flex-col justify-around w-3/5  h-2/6">
-        //                 <input className="text-2xl" type="text" defaultValue={100}/>
-        //                 <input className="text-2xl" type="text" defaultValue={100}/>
-        //             </div>
-        //             <div className="pt-[30px] w-3/5">
-        //                 <ConnectWalletButton/>
-        //             </div>
-        //         </div>
-        //     </div>
-        // </div>
     );
 };
 
--- src/components/DropdownMenu/DropdownMenu.tsx
+++ src/components/DropdownMenu/DropdownMenu.tsx
@@ -1,50 +1,44 @@
 "use client";
 import {useState} from "react";
-import {AiOutlineCaretDown, AiOutlineCaretUp} from "react-icons/ai";
-import {list} from "postcss";
+import {AiOutlineCaretDown, AiOutlineCaretUp} from "react-icons/ai"
+import {ICoin} from "@/types/coin.interface";
 
-export default function DropdownMenu() {
+interface IProps {
+    coinList: ICoin[];
+}
 
-    const list = [
-        {city: "NEW YORK"},
-        {city: "NEW YORK"},
-        {city: "NEW YORK"},
-        {city: "NEW YORK"},
-        {city: "NEW YORK"},
-        {city: "NEW YORK"},
-    ];
+export default function DropdownMenu({coinList}: IProps) {
 
     const [isOpen, setIsOpen] = useState(false);
 
-    console.log(isOpen);
-
-
     return (
-        <div className="relative flex flex-col items-center w-[340px] h-[340px]">
-            <button onClick={() => setIsOpen((prev) => !prev)}
-                    className="bg-blue-300 p-4 w-full flex items-center justify-between font-bold text-lg tracking-wider border-4
+        <div className="absolute top-[468px]">
+            <div className="relative flex flex-col items-center w-[340px] h-[340px] top-8">
+                <button onClick={() => setIsOpen((prev) => !prev)}
+                        className="bg-blue-300 p-4 w-full flex items-center justify-between font-bold text-lg tracking-wider border-2
                     border-transparent active:border-white duration-300 active:text-white"
-            >
-                Dropdown
-                {!isOpen ? (
-                    <AiOutlineCaretDown className="h-8"/>
-                ) : (
-                    <AiOutlineCaretUp className="h-8"/>
-                )}
-            </button>
+                >
+                    Tokens list
+                    {!isOpen ? (
+                        <AiOutlineCaretDown className="h-8"/>
+                    ) : (
+                        <AiOutlineCaretUp className="h-8"/>
+                    )}
+                </button>
 
-            {isOpen && (
-                <div className="bg-blue-400 absolute top-20 w-full">
-                    {list.map((item, index) => (
-                        <div>
-                            <h3>
-                                {item.city}
-                            </h3>
+                    {isOpen && (
+                        <div className=" absolute h-[240px] top-20 w-full overflow-auto">
+                            {coinList.map((item, index) => (
+                                <div key={index}>
+                                    <img className="h-[40px] w-[40px]"
+                                         src={item.image}
+                                         alt=""/>
+                                </div>
+                            ))
+                            }
                         </div>
-                    ))
-                    }
-                </div>
-            )}
+                    )}
+            </div>
         </div>
     );
 };
